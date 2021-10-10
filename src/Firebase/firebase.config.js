b46f4198b0c278c27e067dade47758b93a2f7275
AyuburRahman <aiburrahman17@gmail.com>
2021-10-10 17:01:21 +0600

    final commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRXBba3YT-6fg_YCQuZluhYVH3GZ1jNpI",
+    authDomain: "ema-john-simple-deeac.firebaseapp.com",
+    projectId: "ema-john-simple-deeac",
+    storageBucket: "ema-john-simple-deeac.appspot.com",
+    messagingSenderId: "1054167356024",
+    appId: "1:1054167356024:web:30a40e89753e80e9df538d"
+  };
+
+
+  export default firebaseConfig;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+
+import './Login.css';
+
+const Login = () => {
+    const { signInUsingGoogle } = useAuth();
+    const location = useLocation();
+    const history = useHistory();
+    const redirect_uri = location.state?.from || '/shop';
+
+
+    const handleGoogleLogin = () => {
+        signInUsingGoogle()
+            .then(result => {
+                history.push(redirect_uri);
+            })
+    }
+
+    return (
+        <div className="login-form">
+            <div>
+                <h2>Login</h2>
+                <form>
+                    <input type="email" name="" id="" placeholder="Your Email" />
+                    <br />
+                    <input type="password" name="" id="" placeholder="Enter Password" />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+                <p>New To Ema-John? <Link to="/register">Create Account</Link></p>
+                <div>-------or--------</div>
+                <button
+                    className="btn-regular"
+                    onClick={handleGoogleLogin}
+                >Google Sign In</button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
